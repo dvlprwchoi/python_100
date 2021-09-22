d8f7bb38a85e43f6a231907d01d06af69d2e3815
dvlprwchoi <dvlpr.wchoi@gmail.com>
2021-09-22 09:23:21 -0500

    Complete Day 1

--- day_1_brand_name_generator/day_1.py
+++ day_1_brand_name_generator/day_1.py
@@ -1,6 +1,11 @@
 # 1. Create a greeting for your program.
+print("Welcome to the Brand Name Generator!!")
 # 2. Ask the user for the city that they grew up in.
+city = input("What is the city you did grow up in?\n")
 # 3. Ask the user for the name of a pet.
+pet = input("What is the name of your pet?\n")
 # 4. Combine the name of their city and pet and show them their band name.
+brand_name = city + " " + pet
+print("Your brand name is: \n" + brand_name)
 # 5. Make sure the input cursor shows on a new line, see the example at:
 #   https://band-name-generator-end.appbrewery.repl.run/
