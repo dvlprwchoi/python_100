6256252b45aaa2bd3e2a1afe9d2473725e6baacb
dvlprwchoi <dvlpr.wchoi@gmail.com>
2021-09-24 20:32:28 -0500

    Day 7 step 1

--- day_7_hangman/day_7.py
+++ day_7_hangman/day_7.py
@@ -1,9 +1,22 @@
 # Step 1
+import random
 
 word_list = ["aardvark", "baboon", "camel"]
 
 # TODO-1 - Randomly choose a word from the word_list and assign it to a variable called chosen_word.
 
+chosen_word = random.choice(word_list)
+print(chosen_word)
+
 # TODO-2 - Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
 
+guess = input("What is your guess of a letter?").lower()
+print(guess)
+
 # TODO-3 - Check if the letter the user guessed (guess) is one of the letters in the chosen_word.
+
+for letter in chosen_word:
+    if guess == letter:
+        print("Correct!")
+    else:
+        print("Wrong")
