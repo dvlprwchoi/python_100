4c0bc189402d48ddda70980cdb2adef569abae54
dvlprwchoi <dvlpr.wchoi@gmail.com>
2021-09-27 15:52:47 -0500

    Day 7 step 3

--- day_7_hangman/step_3.py
+++ day_7_hangman/step_3.py
@@ -13,16 +13,26 @@ display = []
 for _ in range(word_length):
     display += "_"
 
+print(display)
+
 # TODO-1: - Use a while loop to let the user guess again. The loop should only stop once the user has guessed all the letters in the chosen_word and 'display' has no more blanks ("_"). Then you can tell the user they've won.
 
-guess = input("Guess a letter: ").lower()
+# Default setting
+game_end = False
+
+while game_end == False:
+    guess = input("Guess a letter: ").lower()
 
 # Check guessed letter
-for position in range(word_length):
-    letter = chosen_word[position]
-    print(
-        f"Current position: {position}\n Current letter: {letter}\n Guessed letter: {guess}")
-    if letter == guess:
-        display[position] = letter
+    for position in range(word_length):
+        letter = chosen_word[position]
+        print(
+            f"Current position: {position}\n Current letter: {letter}\n Guessed letter: {guess}")
+        if letter == guess:
+            display[position] = letter
+    print(display)
 
-print(display)
+    # if letter in display != '_':
+    if not '_' in display:
+        game_end = True
+        print("You won!")
